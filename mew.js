4cb2cabe9b08ee52634dc43216a319f11298c3d8
Glitch (thread-gravity) <none>
2020-01-28 03:56:12 +0000

    🚎🌟 Checkpoint ./mew.js:3513711/9

--- mew.js
+++ mew.js
@@ -107,7 +107,7 @@ if(message.content.includes('มิวต้า')) {
     const webAttachment = new Discord.Attachment('https://cdn.discordapp.com/attachments/556128521437577228/671322770537447429/ezgif-2-6d2428e4edc7.gif')
     message.reply(webAttachment);
    }
-    if(message.content.includes('นมนอย')) {
+    if(message.content.includes('นมนอยู่ตี้หนายๆ')) {
     const webAttachment = new Discord.Attachment('https://cdn.discordapp.com/attachments/556128521437577228/671560735528058880/ezgif-7-ad7eba98051b.gif')
     message.reply(webAttachment);
    }
