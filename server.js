9c83b559d313a488adb343e1b032766561aed587
Glitch (thread-gravity) <none>
2020-06-16 03:53:45 +0000

    🙋🌲 Checkpoint ./server.js:3513711/901

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require("express");
+const http = require('http');
+const express = require('express');
 const app = express();
 
-
 app.get("/", (request, response) => {
-  console.log("Ping received!");
-  response.sendStatus(200);
+console.log(Date.now() + " Ping Received");
+response.sendStatus(200);
 });
 
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.listen(process.env.PORT);
+setInterval(() => {
+http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
